5b53435fee31f9286469ffbc8780be0a572473f8
KindSeven <2845382437@qq.com>
2025-04-07 11:17:17 +0800

    增加了concurrently同时启动所有项目

--- packages/vue-demo1/src/App.vue
+++ packages/vue-demo1/src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import {Button} from '@libc/ui'
 </script>
 
 <template>
@@ -13,6 +14,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <Button>233</Button>
       </nav>
     </div>
   </header>
